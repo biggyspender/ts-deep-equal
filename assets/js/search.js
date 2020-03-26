f09d879cdd74aa2af5212283e46b038787846de8
Chris Sperry <christophermartinsperry@gmail.com>
2020-03-26 19:21:28 +0000

    docs(docs): update gh-pages

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"deepEqual","url":"globals.html#deepequal","classes":"tsd-kind-function"}]};
